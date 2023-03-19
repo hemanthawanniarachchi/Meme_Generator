12b8d3207bf887b38d9df557fac2506d385f3787
hemanthawanniarachchi <hnwanniarachchi98@gmail.com>
2023-03-19 22:40:58 +0530

    adding repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+
+
+import "./styles/App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Meme />
+            
+        </div>
+    );
+}
+
+export default App;
